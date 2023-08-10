e41578040b1d502553825edad9f803f346358efd
unknown <tuliovictorp@hotmail.com>
2023-08-10 19:25:46 -0300

    authentication middlewares and views

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -33,11 +33,50 @@ module.exports = class AuthController {
     const user = { name, email, password: hashedPassword };
 
     try {
-      await User.create(user);
+      const createdUser = await User.create(user);
+
+      req.session.userid = createdUser.id;
+
       req.flash("message", "Cadastro realizado com sucesso!");
-      res.redirect("/");
+
+      req.session.save(() => {
+        res.redirect("/");
+      });
     } catch (err) {
       console.log(err);
     }
+  }
+
+  static logout(req, res) {
+    req.session.destroy();
+    res.redirect("/login");
+  }
+
+  static async loginPost(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email: email } });
+
+    if (!user) {
+      req.flash("message", "Usuário não encontrado");
+      res.render("auth/login");
+      return;
+    }
+
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+
+    if (!passwordMatch) {
+      req.flash("message", "Senha inválida");
+      res.render("auth/login");
+      return;
+    }
+
+    req.session.userid = user.id;
+
+    req.flash("message", "Login realizado com sucesso!");
+
+    req.session.save(() => {
+      res.redirect("/");
+    });
   }
 };
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const ToughtsController = require("../controllers/ToughtsController");
 
+const checkAuth = require("../helpers/auth").checkAuth;
+
+//COLOCAR O MIDDLEWARE APOS TESTE
+
+router.get("/add", ToughtsController.createTought);
+router.get("/dashboard", ToughtsController.dashboard);
 router.get("/", ToughtsController.showToughts);
 
 module.exports = router;
