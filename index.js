8102277f6d006b6c673cfc6905a725119ddd44c4
unknown <tuliovictorp@hotmail.com>
2023-08-13 18:29:19 -0300

    small fixes

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.use(
     }),
     cookie: {
       secure: false,
-      maxAge: 360000,
-      expires: new Date(Date.now() + 360000),
+      maxAge: 3600000,
+      expires: new Date(Date.now() + 3600000),
       httpOnly: true,
     },
   })
